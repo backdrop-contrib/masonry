1efdbc87827819967e0907d2208ab1b715ebe8d9
Dom <dom.clause@gmail.com>
2015-06-25 22:09:38 +0200

    Issue #2022371 by aaronbauman, queenvictoria, zanix, leon.nk, dman, lavamind, linclark, nsciacca: Support for Masonry 3.0.

--- masonry.js
+++ masonry.js
@@ -20,41 +20,51 @@ Drupal.behaviors.masonry = {
       }
       if (settings.column_width) {
         if (settings.column_width_units == 'px') {
-          $options.columnWidth = settings.column_width;
+          $options.columnWidth = parseInt(settings.column_width);
         }
         else if (settings.column_width_units == '%') {
-          $options.columnWidth = function (containerWidth) {
-            return containerWidth * (settings.column_width / 100);
-          };
+          $options.columnWidth = ($container.width() * (settings.column_width / 100)) - settings.gutter_width ;
         }
+        else {
+          $options.columnWidth = settings.column_width;
       }
-      $options.gutterWidth = settings.gutter_width;
-      $options.isResizable = settings.resizable;
-      if (settings.resizable) {
-        $options.isAnimated = settings.animated;
-        if (settings.animated) {
-          $options.animationOptions = {
-            queue: false,
-            duration: settings.animation_duration
-          };
         }
+      if (settings.stamp) {
+        $options.stamp = settings.stamp;
       }
+      $options.gutter = settings.gutter_width;
+      $options.isResizeBound = settings.resizable;
       $options.isFitWidth = settings.fit_width;
-      $options.isRTL = settings.rtl;
+      if (settings.rtl) {
+        $options.isOriginLeft = false;
+      }
+      if (settings.animated) {
+        $options.transitionDuration = settings.animation_duration + 'ms';
+      }
+      else {
+        $options.transitionDuration = 0;
+      }
 
       // Apply Masonry to container
       if (settings.images_first) {
         $container.imagesLoaded(function () {
-          $container.masonry($options);
+          if ($container.hasClass('masonry-processed')) {
+            $container.masonry('reloadItems').masonry('layout');
+          }
+          else {
+            $container.once('masonry').masonry($options);
+          }
         });
       }
       else {
-        $container.masonry($options);
+        if ($container.hasClass('masonry-processed')) {
+          $container.masonry('reloadItems').masonry('layout');
+        }
+        else {
+          $container.once('masonry').masonry($options);
+        }
       }
     });
-
   }
 };
-
 })(jQuery);
-
